bac9b4c4d1ef507a63eb24b9a55475b5a6b02eaf
Kris <Kris@100.70.175.41>
2017-02-21 01:44:40 +0000

    prints opengl version 2

--- src/core/Main.java
+++ src/core/Main.java
@@ -17,6 +17,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println(GL11.glGetString(GL11.GL_VERSION));
 		DisplayManager.createDisplay();
+		System.out.println(GL11.glGetString(GL11.GL_VERSION));
 		Loader loader = new Loader();
 		StaticShader shader = new StaticShader();
 		Renderer renderer = new Renderer(shader);
